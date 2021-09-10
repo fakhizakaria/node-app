460c82e5e0818720f6a3395dae001e3e10987dd1
Fakhi Zakaria <fakhizakaria94@gmail.com>
2021-09-10 08:35:11 +0200

    variable port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send("welcome to the home page");
 });
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("wazap app !!");
 });
